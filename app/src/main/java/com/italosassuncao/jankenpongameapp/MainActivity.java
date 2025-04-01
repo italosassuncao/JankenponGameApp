324ba8fa0982705309c6e33097c75c852fcd2030
Italo Silva de Assunção <83298919+italosassuncao@users.noreply.github.com>
2025-03-31 22:43:27 -0300

    Initial Commit

---
+++ app/src/main/java/com/italosassuncao/jankenpongameapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.italosassuncao.jankenpongameapp;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void selecionarPedra(View view){
+        verificarGanhador("pedra");
+    }
+
+    public void selecionarPapel(View view){
+        verificarGanhador("papel");
+    }
+
+    public void selecionarTesoura(View view){
+        verificarGanhador("tesoura");
+    }
+
+    private void verificarGanhador(String escolhaUsuario){
+        System.out.println("Escolha do usuário: " + escolhaUsuario);
+    }
+}
