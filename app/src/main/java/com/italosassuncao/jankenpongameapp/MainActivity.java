a2b37e9f3d1d572f40f94f4eee945c901ab6b50b
Italo Silva de Assunção <83298919+italosassuncao@users.noreply.github.com>
2025-03-31 22:58:55 -0300

    Add escolha do App

--- app/src/main/java/com/italosassuncao/jankenpongameapp/MainActivity.java
+++ app/src/main/java/com/italosassuncao/jankenpongameapp/MainActivity.java
@@ -2,12 +2,15 @@ package com.italosassuncao.jankenpongameapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,7 +38,38 @@ public class MainActivity extends AppCompatActivity {
         verificarGanhador("tesoura");
     }
 
-    private void verificarGanhador(String escolhaUsuario){
-        System.out.println("Escolha do usuário: " + escolhaUsuario);
+    private String gerarEscolhaApp() {
+
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+        int numeroAleatorio = new Random().nextInt(3);
+
+        ImageView imageViewApp = findViewById(R.id.image_app);
+        String escolhaApp = opcoes[numeroAleatorio];
+        switch (escolhaApp){
+            case "pedra":
+                imageViewApp.setImageResource(R.drawable.pedra);
+                break;
+
+            case "papel":
+                imageViewApp.setImageResource(R.drawable.papel);
+                break;
+
+            case "tesoura":
+                imageViewApp.setImageResource(R.drawable.tesoura);
+                break;
+        }
+        return escolhaApp;
+
+        /*String[] opcoes = {"pedra", "papel", "tesoura"};
+        int indice = (int) (Math.random() * opcoes.length);
+        return opcoes[indice];*/
     }
+
+    private void verificarGanhador(String escolhaUsuario){
+
+        String escolhaApp = gerarEscolhaApp();
+
+        //System.out.println("Escolha do usuário: " + escolhaUsuario);
+    }
+
 }
